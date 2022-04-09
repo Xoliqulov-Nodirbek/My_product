d89e6f44cc6b12294ca31a2d4891bb7f113f052d
Xoliqulov-Nodirbek <xoliqulov.nodirbek2003@gmail.com>
2022-04-09 23:01:32 +0500

    Added opened text

--- js/main.js
+++ js/main.js
@@ -3,6 +3,42 @@ let body = document.querySelector("body");
 let elBtn = document.querySelector(".header__menu");
 let elHeader = document.querySelector(".header__inner");
 
+// ----- > One
+
+let textOne = document.querySelector(".question__content-text1");
+let btnOne = document.querySelector(".question__content-btn1");
+
+btnOne.addEventListener("click", function(){
+    textOne.classList.toggle("question__content-text1--open")
+});
+
+// ----- > Two
+
+let textTwo = document.querySelector(".question__content-text2");
+let btnTwo = document.querySelector(".question__content-btn2");
+
+btnTwo.addEventListener("click", function(){
+    textTwo.classList.toggle("question__content-text2--open")
+});
+
+// ----- > Three
+
+let textThree = document.querySelector(".question__content-text3");
+let btnThree = document.querySelector(".question__content-btn3");
+
+btnThree.addEventListener("click", function(){
+    textThree.classList.toggle("question__content-text3--open")
+});
+
+// ----- > Four
+
+let textFour = document.querySelector(".question__content-text4");
+let btnFour = document.querySelector(".question__content-btn4");
+
+btnFour.addEventListener("click", function(){
+    textFour.classList.toggle("question__content-text4--open")
+});
+
 // --> Light Mode and Dark Mode part in header.
 
 changerButton.addEventListener("click", function(){
@@ -14,3 +50,5 @@ changerButton.addEventListener("click", function(){
 elBtn.addEventListener("click", function(){
     elHeader.classList.toggle("header__inner-open");
 });
+
+
